f1c1ceb73f478484ba263b75fa7a6fc35e05e9e1
Thongrob Tipsurat <thongrob.tsr@gmail.com>
2022-05-19 22:12:12 +0700

    Edit Navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -34,7 +34,7 @@ function Navbar() {
             </li>
             <li className="nav-item dropdown">
                     <NavLink className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        จัดการรายการสินค้า
+                        จัดการรายการGU
                     </NavLink>
                     <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                         <li><NavLink className="dropdown-item" to="/insert">เพิ่มรายการ</NavLink></li>
